dde30f43e34d68fc03f6622e2c4406fec6af7371
Nabagata <ngsaha234@gmail.com>
2018-10-07 15:23:08 +0530

    schedulingAlgo

---
+++ PthreadExample.c
@@ -0,0 +1,31 @@
+#include<pthread.h>
+#include<stdio.h>
+
+int sum=0;
+
+void runner(void *param)
+{
+  int n=atoi(param);
+  for(int i=0;i<n;i++)
+    sum+=i;
+  pthread_exit(0);
+}
+
+int main()
+{
+  pthread_t tid;
+  pthread_attr_t attr;
+
+  if(argc!=2){
+    printf("Usage: a.out <int>");
+    return -1;
+  }
+  if(atoi(argv[1])<0){
+    printf("Enter positive number");
+  }
+  pthread_attr_init(&attr);
+  pthread_create(&tid,&attr,runner,argv[1]);
+  pthread_join(tid,NULL);
+
+  printf("Sum= %d",sum);
+}
